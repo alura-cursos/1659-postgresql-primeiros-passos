b4f26582c263e7cad97499ce97d89e77951d7925
Rômulo Henrique <romulo.henriquemv@gmail.com>
2021-12-02 15:47:50 -0300

    Add files via upload

---
+++ Aula01-atividade05.sql
@@ -0,0 +1,16 @@
+CREATE TABLE aluno
+(
+    id SERIAL,
+    nome  VARCHAR(255),
+    cpf CHAR(11),
+    observacao TEXT,
+    idade INTEGER,
+    dinheiro NUMERIC(10,2),
+    altura REAL,
+    ativo BOOLEAN,
+    data_nascimento DATE,
+    hora_aula TIME,
+    matriculado_em TIMESTAMP
+);
+
+SELECT * FROM aluno;
